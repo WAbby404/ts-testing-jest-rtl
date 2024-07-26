8f115b7b1f6ac208a7693f62f130d1585366b7ae
Abby Waddell <wabby.404@gmail.com>
2024-07-26 15:17:10 -0400

    more tests

--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -1,14 +1,22 @@
 import { render, screen } from "@testing-library/react";
 import { Greet } from "./Greet";
 
-test("Greet renders correctly", () => {
-  render(<Greet />);
-  const textElement = screen.getByText(/hello/i);
-  expect(textElement).toBeInTheDocument();
-});
+describe("Greet", () => {
+  test("renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
 
-test("Greet renders with a name", () => {
-  render(<Greet name="Abby" />);
-  const textElement = screen.getByText("Hello Abby");
-  expect(textElement).toBeInTheDocument();
+  it.only("renders a name", () => {
+    render(<Greet name="Abby" />);
+    const textElement = screen.getByText("Hello Abby");
+    expect(textElement).toBeInTheDocument();
+  });
+
+  it.skip("renders an Abbyname", () => {
+    render(<Greet name="Abbyname" />);
+    const textElement = screen.getByText("Hello Abbyname");
+    expect(textElement).toBeInTheDocument();
+  });
 });
